0041d72d5fcbc9b815d5ac0b6de3d17a176b1805
Matthew <matthewzammit@gmail.com>
2016-12-30 23:03:39 +0100

    card 13

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ app.post('/webhook', function (req, res) {
         if (event.message && event.message.text) {
 			if (event.message.text === 'Generic') {
 				sendGenericMessage(event.sender.id)
-				continue;
+				continue
             }
 			
 			else if (!kittenMessage(event.sender.id, event.message.text)) {
@@ -112,6 +112,7 @@ function sendGenericMessage(sender) {
 	
 	sendMessage(sender, {text: "GENERIC FUNCTION CALL"});	
 
+/*
 	messageData = {
         "attachment": {
             "type": "template",
@@ -136,6 +137,7 @@ function sendGenericMessage(sender) {
     }
     
 	sendMessage(recipientId, messageData);
+*/
 
 	
 	
