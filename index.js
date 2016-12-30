0067b16e0eb1c2b61a5c6d2dd1a185103d95e4c5
Matthew <matthewzammit@gmail.com>
2016-12-31 00:40:59 +0100

    card 22

--- index.js
+++ index.js
@@ -94,26 +94,42 @@ function kittenMessage(recipientId, text) {
                                 "payload": "User " + recipientId + " likes kitten " + imageUrl,
                             }]
                         }, {
-                    "title": "Oculus rift",
-                    "subtitle": "Step into Rift. Whether you’re stepping into your favorite game, watching an immersive VR movie, jumping to a destination on the other side of the world, or just spending time with friends in VR, you’ll feel like you’re really there.",
-                    "image_url": "http://messengerdemo.parseapp.com/img/rift.png",
+                    "title": "Amazon Echo, Black",
+                    "subtitle": "Step into Rift. Whether you’re stepping into your favorite game, watching an immersive VR movie, jumping to a destination on the other side of the world, or just spending time with friAmazon Echo is a hands-free speaker you control with your voice. Echo connects to the Alexa Voice Service to play music, provide information, news, sports scores, weather and more. Prime members can also ask Alexa to order eligible products they've ordered before and many Prime products. All you have to do is ask.",
+                    "image_url": "https://images-na.ssl-images-amazon.com/images/I/61aN%2BSE-F9L._SL1000_.jpg",
                     "buttons": [{
                         "type": "web_url",
-                        "url": "https://www.amazon.co.uk/d/PC-Video-Games/Oculus-301-00204-01-Rift/B00ZFOGHRG/ref=sr_1_1?ie=UTF8&qid=1483140062&sr=8-1&keywords=rift",
+                        "url": "https://www.amazon.co.uk/Amazon-SK705DI-Echo-Black/dp/B01GAGVIE4/ref=sr_1_1",
                         "title": "Buy now"
                     }, {
                         "type": "postback",
                         "title": "Postback",
-                        "payload": "Payload for first element in a generic bubble",
+                        "payload": "Amazon Echo",
                     }],
                 }, {
-                    "title": "Samsung Gear Gen 2 Virtual Reality",
-                    "subtitle": "With the Gear VR you get a bigger field of view, smoother images. Low light leakage and reflection prevention. It's game on.",
-                    "image_url": "https://images-na.ssl-images-amazon.com/images/I/5137IxTJdlL._SY355_.jpg",
+                    "title": "Oculus Rift",
+                    "subtitle": "Step into Rift. Whether you’re stepping into your favorite game, watching an immersive VR movie, jumping to a destination on the other side of the world, or just spending time with friends in VR, you’ll feel like you’re really there.",
+                   "image_url": "https://images-na.ssl-images-amazon.com/images/I/41iVakANl8L._SX425_.jpg",
                     "buttons": [{
+                        "type": "web_url",
+                        "url": "https://www.amazon.co.uk/d/PC-Video-Games/Oculus-301-00204-01-Rift/B00ZFOGHRG/ref=sr_1_1",
+                        "title": "Buy now"
+                    }, {
                         "type": "postback",
-                        "title": "Buy now",
-                        "payload": "Payload for second element in a generic bubble",
+                        "title": "Postback",
+                        "payload": "Oculus rift",
+                    }, {
+                    "title": "Philips Hue White and Colour Ambiance Wireless Lighting",
+                    "subtitle": "Limitless possibilities - connected to you. Get started with the Philips Hue white and colour ambience starter kit and experience high quality white and coloured light that offers you endless possibilities. Play with light and choose from 16 million colours in the Hue app to match the light to your mood. The bulbs have been improved with deeper green, cyan and blue for even better scene setting in your home. Use a favourite photo and re-live that special moment with splashes of light. This Philips Hue bridge is compatible with Apple Home Kit technology. Ask Siri to turn on or dim your lights or recall pre-sets without touching a single button. Use a favourite photo and re-live that special moment with splashes of light. Save your favourite light settings and recall them whenever you want with the tap of a finger.",
+                   "image_url": "https://images-na.ssl-images-amazon.com/images/I/71me8OodAxL._SL1500_.jpg",
+                    "buttons": [{
+                        "type": "web_url",
+                        "url": "https://www.amazon.co.uk/Philips-Ambiance-Wireless-Lighting-Starter/dp/B01K1WP7Z4/ref=sr_1_1",
+                        "title": "Buy now"
+                    }, {
+                        "type": "postback",
+                        "title": "Postback",
+                        "payload": "Philips Hue",
                     }],
                 }]
             }
