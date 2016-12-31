b167ab5fac853d28f157ff2cf807d5bc9389d31e
Matthew <matthewzammit@gmail.com>
2016-12-31 04:46:43 +0100

    Positive kitten 2

--- xxindex.js
+++ xxindex.js
@@ -31,14 +31,20 @@ app.post('/webhook', function (req, res) {
 				sendGenericMessage(event.sender.id);
                 //continue;
             }			
-			else if (!kittenMessage(event.sender.id, event.message.text)) {
+
+			else if (values.length === 3 && values[0].toUpperCase() === 'KITTEN'){
+				kittenMessage(event.sender.id, event.message.text);
+			}
+			/*			else if (!kittenMessage(event.sender.id, event.message.text)) {
 				sendMessage(event.sender.id, {text: "Echo: " + event.message.text});
 			}
-			
+			*/
         } else if (event.postback) {
                 console.log("Postback received: " + JSON.stringify(event.postback));
             sendMessage(event.sender.id, {text: "I like this kitten too!"});
-        }
+        } else {
+			sendMessage(event.sender.id, {text: "Echo: " + event.message.text}); //Echo back
+		}
     }
     res.sendStatus(200);
 });
@@ -62,7 +68,57 @@ function sendMessage(recipientId, message) {
     });
 };
 
+
 // send rich message with kitten
+function kittenMessage(recipientId, text) {
+
+    text = text || "";
+    var values = text.split(' ');
+sendMessage(event.sender.id, {text: "Echo: " + text}); //Echo back
+        if (Number(values[1]) > 0 && Number(values[2]) > 0) {
+
+            var imageUrl = "https://placekitten.com/g/" + Number(values[1]) + "/" + Number(values[2]);
+
+            message = {
+                "attachment": {
+                    "type": "template",
+                    "payload": {
+                        "template_type": "generic",
+                        "elements": [{
+                            "title": "Kitten",
+                            "subtitle": "Cute kitten picture",
+                            "image_url": imageUrl ,
+                            "buttons": [{
+                                "type": "web_url",
+                                "url": imageUrl,
+                                "title": "Show kitten"
+                                }, 
+										{
+                                "type": "postback",
+                                "title": "I like this",
+                                "payload": "User " + recipientId + " likes kitten " + imageUrl,
+                            }]
+                        }]
+            }
+        }
+    }
+            sendMessage(recipientId, message);
+
+      //      return true;
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+// send rich message with kitten
+/*
 function kittenMessage(recipientId, text) {
 
     text = text || "";
@@ -145,56 +201,8 @@ function kittenMessage(recipientId, text) {
     return false;
 
 }
+*/
 
 function sendGenericMessage(sender) {
-	
-	                sendMessage(sender, {text: "GENERIC FUNCTION CALL"});	
-
-	
-/*    messageData = {
-        "attachment": {
-            "type": "template",
-            "payload": {
-                "template_type": "generic",
-                "elements": [{
-                    "title": "First card",
-                    "subtitle": "Element #1 of an hscroll",
-                    "image_url": "http://messengerdemo.parseapp.com/img/rift.png",
-                    "buttons": [{
-                        "type": "web_url",
-                        "url": "https://www.messenger.com",
-                        "title": "web url"
-                    }, {
-                        "type": "postback",
-                        "title": "Postback",
-                        "payload": "Payload for first element in a generic bubble",
-                    }],
-                }, {
-                    "title": "Second card",
-                    "subtitle": "Element #2 of an hscroll",
-                    "image_url": "http://messengerdemo.parseapp.com/img/gearvr.png",
-                    "buttons": [{
-                        "type": "postback",
-                        "title": "Postback",
-                        "payload": "Payload for second element in a generic bubble",
-                    }],
-                }]
-            }
-        }
-    }
-    request({
-        url: 'https://graph.facebook.com/v2.6/me/messages',
-        qs: {access_token:token},
-        method: 'POST',
-        json: {
-            recipient: {id:sender},
-            message: messageData,
-        }
-    }, function(error, response, body) {
-        if (error) {
-            console.log('Error sending messages: ', error)
-        } else if (response.body.error) {
-            console.log('Error: ', response.body.error)
-        }
-    })*/
+	sendMessage(sender, {text: "GENERIC FUNCTION CALL"});	
 };
