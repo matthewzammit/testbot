0c62df4dca1b05f1ebf4f4e55313f21411075b99
Matthew <matthewzammit@gmail.com>
2016-12-31 05:21:07 +0100

    Positive kitten 5

--- index.js
+++ index.js
@@ -91,7 +91,7 @@ function kittenMessage(recipientId, text) {
     text = text || "";
     var values = text.split(' ');
 
-    if (values.length === 3 && values[0].toUpperCase() === 'KITTEN') {
+  //  if (values.length === 3 && values[0].toUpperCase() === 'KITTEN') {
         if (Number(values[1]) > 0 && Number(values[2]) > 0) {
 
             var imageUrl = "https://placekitten.com/g/" + Number(values[1]) + "/" + Number(values[2]);
@@ -117,7 +117,7 @@ function kittenMessage(recipientId, text) {
                         }]
             }
         }
-    }
+ //  }
             sendMessage(recipientId, message);
         }
 }
